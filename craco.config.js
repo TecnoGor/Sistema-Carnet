313c1df31553b05928d3ad1ca8cbb0370be3daf7
TecnoGor <130939084+TecnoGor@users.noreply.github.com>
2025-07-10 10:58:48 -0400

    Cambios en craco y En package

--- craco.config.js
+++ craco.config.js
@@ -5,16 +5,20 @@ module.exports = {
     configure: (webpackConfig) => {
       webpackConfig.devtool = false;
       // Elimina la restricción de importación fuera de /src
-      webpackConfig.plugins.forEach(plugin => {
-        if (plugin.definitions && plugin.definitions['process.env']) {
-          plugin.definitions['process.env.NODE_ENV'] = JSON.stringify('production');
-          delete plugin.definitions['process.env'].NODE_ENV; // Elimina duplicados
-        }
-      });
-      webpackConfig.stats = {
-        children: false,
-        warningsFilter: [/Conflicting values for 'process.env.NODE_ENV'/]
+
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        "fs": false,
+        "path": require.resolve("path-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify")
       };
+      
+      // Actualiza para nueva versión de webpack
+      webpackConfig.ignoreWarnings = [
+        { module: /node_modules\/stylis-plugin-rtl/ },
+        { module: /node_modules\/@tensorflow/ }
+      ];
       webpackConfig.module.rules = webpackConfig.module.rules.filter(
         (rule) => !rule.loader?.includes('source-map-loader')
       );
