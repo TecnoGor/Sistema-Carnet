56b17f5e7048207614a53eed7cb4247129bf3681
TecnoGor <130939084+TecnoGor@users.noreply.github.com>
2025-07-10 10:43:32 -0400

    Cambios en el archivo craco

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,19 @@ module.exports = {
     configure: (webpackConfig) => {
       webpackConfig.devtool = false;
       // Elimina la restricción de importación fuera de /src
+      webpackConfig.plugins.forEach(plugin => {
+        if (plugin.definitions && plugin.definitions['process.env']) {
+          plugin.definitions['process.env.NODE_ENV'] = JSON.stringify('production');
+          delete plugin.definitions['process.env'].NODE_ENV; // Elimina duplicados
+        }
+      });
+      webpackConfig.stats = {
+        children: false,
+        warningsFilter: [/Conflicting values for 'process.env.NODE_ENV'/]
+      };
+      webpackConfig.module.rules = webpackConfig.module.rules.filter(
+        (rule) => !rule.loader?.includes('source-map-loader')
+      );
       webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
         (plugin) => plugin.constructor.name !== "ModuleScopePlugin"
       );
