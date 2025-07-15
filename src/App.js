1cd6da279fdcf6beddca2188a3f6315ffdb0f334
TecnoGor <130939084+TecnoGor@users.noreply.github.com>
2025-07-15 10:45:42 -0400

    Cambios en sintaxis

--- src/App.js
+++ src/App.js
@@ -44,9 +44,9 @@ export default function App() {
 
   useEffect(() => {
     // Intentar una conexión simple al cargar la app
-    fetch('https://10.16.12.47:5001/healthcheck', { 
-      mode: 'no-cors',
-      cache: 'no-store'
+    fetch("https://10.16.12.47:5001/healthcheck", { 
+      mode: "no-cors",
+      cache: "no-store",
     }).catch(() => {}); // Ignorar errores intencionalmente
   }, []);
 
